e49511bbf1c97cf5816e30e867b600cef5f8e1f0
LarsRydberg <153441378+LarsRydberg@users.noreply.github.com>
2024-03-14 10:14:15 +0100

    Update WC.java

--- src/main/java/com/wigellProjekt/WC.java
+++ src/main/java/com/wigellProjekt/WC.java
@@ -7,4 +7,47 @@ public class WC {
     private String name;
     private String concert;
     private ArrayList<String> customers;
+
+    public WC () {
+
+    }
+
+    public WC(int id, String name, String concert, ArrayList<String> customers) {
+        this.id = id;
+        this.name = name;
+        this.concert = concert;
+        this.customers = customers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConcert() {
+        return concert;
+    }
+
+    public void setConcert(String concert) {
+        this.concert = concert;
+    }
+
+    public ArrayList<String> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<String> customers) {
+        this.customers = customers;
+    }
 }
